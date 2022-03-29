774ed05ba1c04aa04017e164d338e3a28e510929
Stefa0421 <stefa.pacchin@gmail.com>
2022-03-29 17:51:46 +0200

    Sus

--- src/GestoreCorsiJava1.java
+++ src/GestoreCorsiJava1.java
@@ -4,6 +4,7 @@ public class GestoreCorsiJava1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ciao");
+		System.out.println("Ciao Dallol");
 	}
 
 }
