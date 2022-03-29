d4bc06b6cc9d7561b65d2f5500606644425368dd
HDallol <marcodallopolveni@gmail.com>
2022-03-29 22:55:35 +0200

    Non lo so

--- src/GestoreCorsiJava1.java
+++ src/GestoreCorsiJava1.java
@@ -6,12 +6,31 @@ public class GestoreCorsiJava1 {
 		System.out.println("Ciao");
 		System.out.println("Ciao Dallol");
 		System.out.println("Ciao pac");
+
 		System.out.println("sjkldfhjksdfgjhkdfghjkldfgshjklsdfg");
 		System.out.println("dfkljghsdfjklgh");
 	
 		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
 	
 	
+
+
+		
+		//Un ciclo for
+		for(int i=0;i<5;i++) {
+			String s = "G"+i;
+			System.out.println(s);
+		}
+		
+		int x = (int) Math.pow(3, 4);
+
+		System.out.println("Solosod");
+		
+		for(int i=0;i<11;i++) {
+			System.out.println("LOL");
+		}
+		
+
 	}
 
 }
