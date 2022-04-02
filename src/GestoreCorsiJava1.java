24b6974ea8d41eb402d0608e6196a1a8eb7f55fb
Stefa0421 <stefa.pacchin@gmail.com>
2022-04-02 20:19:40 +0200

    niente se vuoi vai a vedere come si nominano le versioni dei software durante lo sviluppo

--- src/GestoreCorsiJava1.java
+++ src/GestoreCorsiJava1.java
@@ -1,7 +1,7 @@
 /*
  * Autori: Marco Dall'O' Polveni, Stefano Pacchin
  * 
- * Versione: Pre-Alpha 1.0.1
+ * Versione: Pre-Alpha 0.0.1
  *  
  *  Un programma che permette la gestione dei corsi di formazione
  *  di tutti i dipendenti di un'azienda
@@ -21,7 +21,7 @@ public class GestoreCorsiJava1 extends JFrame{
 	Dimension dimensioniFinestra = new Dimension(dimensioniSchermo.width/2, dimensioniSchermo.height/2);
 	
 	public GestoreCorsiJava1() {
-		super("Gestore Corsi di Formazione - Pre-Alpha 1.0.1");
+		super("Gestore Corsi di Formazione - Pre-Alpha 0.0.1");
 		
 		
 		this.setSize(dimensioniFinestra);
